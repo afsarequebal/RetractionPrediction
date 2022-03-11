bae97538b8e65f975f540eb2c69514a2d33b559b
afsarequebal <afsarequebal@gmail.com>
2022-03-11 12:59:51 -0500

    csv

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 from bs4 import BeautifulSoup
-import requests
-
+import numpy as np
+import csv
 
 def scrape():
     with open('retraction.html') as html_file:
         soup = BeautifulSoup(html_file, 'lxml')
-
+    reasonArr = []
+    filteredReasonArr=[]
+    retractedDoiArr = []
+    filteredRetractedDoiArr = []
+    originalDate = []
+    filteredOriginalDate = []
+    retractedDate = []
+    filteredRetractedDate = []
+    articleType = []
+    filteredarticleType = []
     retractionTable = soup.find('table', id='grdRetraction')
     for paper in retractionTable.find_all('tr', class_='mainrow'):
         tdx = paper.find_all('td')
-        reason = ", ".join([str(item.text) for item in tdx[2].find_all('div', class_='rReason')])
-        print(reason)
-        #for r in reason:
-         #   print(r.text)
-        print(tdx[4].find('span', class_='rNature').text)
-        print(tdx[5].find('span', class_='rNature').text)
-        # for i in range(1, len(tdx)):
-        #   print(tdx[i])
-        print('')
+        if tdx[4].find('span', class_='rNature').text != 'unavailable':
+
+            reason = ", ".join([str(item.text) for item in tdx[2].find_all('div', class_='rReason')])
+            reasonArr.append(reason)
+            #print(reason)
+
+            # original doi
+            #print(tdx[4].find('span', class_='rNature').text)
+            retractedDoiArr.append(tdx[4].find('span', class_='rNature').text)
+            originalDate.append(tdx[4].contents[0])
+            retractedDate.append(tdx[5].contents[0])
+            articleType.append(tdx[6].contents[0])
+        # retracted doi
+        #print(tdx[5].find('span', class_='rNature').text)
+
+
+        #print('')
+   # np1 = np.array(retractedDoiArr)
+   # np2 = np.array(reasonArr)
+    #np.reshape(retractedDoiArr, -1)
+    #np.reshape(reasonArr, -1)
+   # print(np1.shape)
+   # print(np2.shape)
+    #a = np.array([retractedDoiArr, reasonArr])
+   # a = np.array(np1, np2)
+
+    with open('retractedPapers.csv', 'w', newline='') as file:
+        mywriter = csv.writer(file, delimiter=',')
+        mywriter.writerows(zip(retractedDoiArr, reasonArr,
+                               originalDate, retractedDate, articleType))
 
 
 scrape()
